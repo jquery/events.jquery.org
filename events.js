4a9e7bcb19377e96f508fd67104f031810004b6f
Scott González <scott.gonzalez@gmail.com>
2014-08-22 09:35:21 -0400

    Add all past events to the event list

--- events.js
+++ events.js
@@ -49,7 +49,34 @@ module.exports = [
 		url: "http://www.gentics.com/jquery-eu-2014/page/2014/eu.html",
 		end: new Date( "March 1, 2014" )
 	},
-
+	{
+		title: "jQueryTO 2014",
+		location: "Toronto, Canada",
+		date: "Mar 15 - 16 2014",
+		url: "http://jqueryto.com/",
+		end: new Date( "March 16, 2014" )
+	},
+	{
+		title: "jQuery Virtual Training: April",
+		location: "Online",
+		date: "April 16 - 17 2014",
+		url: "http://appendto.com/what-we-do/training/schedule/jquery-virtual-training-april-2014/",
+		end: new Date( "April 17, 2014" )
+	},
+	{
+		title: "jQuery UK 2014",
+		location: "Oxford, UK",
+		date: "May 16 2014",
+		url: "http://jqueryuk.com/?utm_source=jqueryorg&utm_medium=banner&utm_campaign=jqueryorgbanner",
+		end: new Date( "May 16, 2014" )
+	},
+	{
+		title: "jQuery Virtual Training: June",
+		location: "Online",
+		date: "June 17 - 19 2014",
+		url: "http://appendto.com/what-we-do/training/schedule/jquery-virtual-training-june-2014/",
+		end: new Date( "June 19, 2014" )
+	},
 	{
 		title: "jQuery Europe 2013",
 		location: "Vienna, Austria",
@@ -64,12 +91,186 @@ module.exports = [
 		url: "http://jqueryto.com/2013",
 		end: new Date( "March 3, 2013" )
 	},
-
+	{
+		title: "jQuery UK 2013",
+		location: "Oxford, UK",
+		date: "Apr 19 2013",
+		url: "http://events.jquery.org/2013/uk/",
+		end: new Date( "April 19, 2013" )
+	},
+	{
+		title: "jQuery Virtual Training: UK",
+		location: "Online",
+		date: "May 21 - 23 2013",
+		url: "http://appendto.com/training/jquery-virtual-training-uk/",
+		end: new Date( "May 23, 2013" )
+	},
+	{
+		title: "jQuery Conference Portland",
+		location: "Portland, OR",
+		date: "June 13 - 14 2013",
+		url: "/2013/portland/",
+		end: new Date( "June 14, 2013" )
+	},
+	{
+		title: "jQuery Virtual Training: July",
+		location: "Online",
+		date: "July 23 - 25 2013",
+		url: "http://appendto.com/training/jquery-virtual-training-july-2013/",
+		end: new Date( "July 25, 2013" )
+	},
+	{
+		title: "jQuery Conference Austin 2013",
+		location: "Austin Convention Center",
+		date: "Sep 10 - 11 2013",
+		url: "/2013/austin/",
+		end: new Date( "September 11, 2013" )
+	},
+	{
+		title: "jQuery Accessibility Summit",
+		location: "Online",
+		date: "Oct 10 - 11 2013",
+		url: "http://www.deque.com/deque-partners-jquery-create-accessibility-summit",
+		end: new Date( "October 11, 2013" )
+	},
+	{
+		title: "jQuery Russia 2013",
+		location: "Digitial October",
+		date: "Oct 15, 2013",
+		url: "http://itmozg.ru/",
+		end: new Date( "October 15, 2013" )
+	},
+	{
+		title: "jQuery Virtual Training: October",
+		location: "Online",
+		date: "Oct 15 - 17 2013",
+		url: "http://modernweb.com/training/jquery-oct-2013.php",
+		end: new Date( "October 17, 2013" )
+	},
+	{
+		title: "CSS Dev Conf",
+		location: "Stanley Hotel, Colorado",
+		date: "Oct 21 - 22 2013",
+		url: "http://2013.cssdevconf.com/",
+		end: new Date( "October 22, 2013" )
+	},
+	{
+		title: "jQuery Virtual Training: November",
+		location: "Online",
+		date: "Nov 12 - 14 2013",
+		url: "http://modernweb.com/training/jquery-nov-2013.php",
+		end: new Date( "November 14, 2013" )
+	},
+	{
+		title: "JavaScript Summit",
+		location: "Online",
+		date: "Nov 19 - 21 2013",
+		url: "http://javascriptsummit.com/",
+		end: new Date( "November 21, 2013" )
+	},
+	{
+		title: "jQuery Virtual Training: December",
+		location: "Online",
+		date: "Dec 17 - 19 2013",
+		url: "http://modernweb.com/training/jquery-dec-2013.php",
+		end: new Date( "December 19, 2013" )
+	},
 	{
 		title: "jQuery UK 2012",
 		location: "Oxford, UK",
 		date: "Feb 10 2012",
 		url: "http://events.jquery.org/2012/uk/",
 		end: new Date( "February 10, 2012" )
+	},
+	{
+		title: "jQuery Bowling @ King Pinz",
+		location: "Leesburg, VA",
+		date: "Apr 13 2012 7-11pm",
+		url: "http://blog.jquery.com/2012/03/26/bowling-for-jquery-apr-2012-dc/",
+		end: new Date( "April 13, 2012" )
+	},
+	{
+		title: "jQuery Conference SF 2012",
+		location: "San Francisco, CA",
+		date: "June 28 - 29 2012",
+		url: "/2012/sf/",
+		end: new Date( "June 29, 2012" )
+	},
+	{
+		title: "jQuery Bowling @ King Pinz",
+		location: "Leesburg, VA",
+		date: "Oct 15 2012 7-11pm",
+		url: "/2012/developer-summit/index.php#bowling",
+		end: new Date( "October 15, 2012" )
+	},
+	{
+		title: "jQuery Developer Summit",
+		location: "Dulles, VA",
+		date: "Oct 15 - 16 2012",
+		url: "/2012/developer-summit/",
+		end: new Date( "October 16, 2012" )
+	},
+	{
+		title: "jQuery Conference Asia",
+		location: "Seoul, Korea",
+		date: "Nov 12 2012",
+		url: "http://www.jqueryasia.com/",
+		end: new Date( "November 12, 2012" )
+	},
+	{
+		title: "jQuery Conf. SF Bay Area 2011",
+		location: "Mountain View, CA",
+		date: "Apr 16 - 17 2011",
+		url: "/2011/sf-bay-area/",
+		end: new Date( "April 17, 2011" )
+	},
+	{
+		title: "jQuery Conference Boston 2011",
+		location: "Boston, MA",
+		date: "Oct 1 - 2 2011",
+		url: "/2011/boston/",
+		end: new Date( "October 2, 2011" )
+	},
+	{
+		title: "14 Days of jQuery",
+		location: "jquery14.com",
+		date: "Jan 14 - 27 2010",
+		url: "http://jquery14.com/",
+		end: new Date( "January 27, 2010" )
+	},
+	{
+		title: "jQuery Conf. SF Bay Area 2010",
+		location: "Mountain View, CA",
+		date: "Apr 24 - 25 2010",
+		url: "/2010/sf-bay-area/",
+		end: new Date( "April 25, 2010" )
+	},
+	{
+		title: "jQuery Conference Boston 2010",
+		location: "Boston, MA",
+		date: "Oct 16 - 17 2010",
+		url: "/2010/boston/",
+		end: new Date( "Octobe 17, 2010" )
+	},
+	{
+		title: "jQuery Conference 2009",
+		location: "Boston, MA",
+		date: "Sep 12 - 13 2009",
+		url: "http://web.archive.org/web/20100418222819/http://events.jquery.com/jquery-conference-2009/",
+		end: new Date( "September 13, 2009" )
+	},
+	{
+		title: "jQuery Conference 2008",
+		location: "Boston, MA",
+		date: "Sep 28, 2008",
+		url: "http://blog.jquery.com/2008/08/31/jquery-conference-2008-agenda/",
+		end: new Date( "September 28, 2008" )
+	},
+	{
+		title: "jQuery Conference 2007",
+		location: "Boston, MA",
+		date: "Oct 27 2007",
+		url: "http://events.jquery.org/2007/boston/",
+		end: new Date( "October 27, 2007" )
 	}
 ];
